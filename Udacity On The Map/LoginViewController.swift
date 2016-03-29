bbd3891fd8a96edd34a8e9ef3f105d162cc8d5fb
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-03-29 13:43:33 +0200

    Initial commit

--- Udacity On The Map/LoginViewController.swift
+++ Udacity On The Map/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Udacity On The Map
 //
 //  Created by Sandra Adams-Hallie on Mar/29/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
