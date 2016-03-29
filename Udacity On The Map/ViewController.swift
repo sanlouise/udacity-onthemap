2b6629e5661b3d48c04fcae13a520d63c2b85486
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-03-29 13:19:20 +0200

    Initial Commit

---
+++ Udacity On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udacity On The Map
+//
+//  Created by Sandra Adams-Hallie on Mar/29/16.
+//  Copyright © 2016 Sandra Adams-Hallie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
